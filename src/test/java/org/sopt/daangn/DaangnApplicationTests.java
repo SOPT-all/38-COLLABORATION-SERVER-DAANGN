5fdfde00c0335a842a95b6f9c57b864af6322091
kymin <park48664866@gmail.com>
2026-05-06 23:12:12 +0900

    project setting

---
+++ src/test/java/org/sopt/daangn/DaangnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.daangn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaangnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
